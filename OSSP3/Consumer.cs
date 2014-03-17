e628e5bf2208d20669f94c359e730608a3a69fdb
Mikhail Bulash <scaryspiderpig@gmail.com>
2014-03-17 16:24:01 +0300

    Consumer template.

--- OSSP3/Consumer.cs
+++ OSSP3/Consumer.cs
@@ -1,11 +1,25 @@
 using System;
+using System.Collections.Concurrent;
 
 namespace OSSP3
 {
     public class Consumer
     {
-        public Consumer()
+        BlockingCollection<string> fileQueue;
+
+        public Consumer(BlockingCollection<string> queue)
         {
+            fileQueue = queue;
+        }
+
+        public void Run()
+        {
+            string filename;
+            while(fileQueue.TryTake(out filename))
+            {
+                // doSmth(filename);
+                // Console.WriteLine(filename);
+            }
         }
     }
 }
