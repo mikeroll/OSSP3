cd2f3a2f53020cd3fbed11ba2956f8b7bf1fb632
Mikhail Bulash <scaryspiderpig@gmail.com>
2014-03-18 03:38:20 +0300

    Consumer complete

--- OSSP3/Consumer.cs
+++ OSSP3/Consumer.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Concurrent;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace OSSP3
 {
@@ -17,9 +19,45 @@ namespace OSSP3
             string filename;
             while(fileQueue.TryTake(out filename))
             {
-                // doSmth(filename);
-                // Console.WriteLine(filename);
+                int mzs = CountMZs(filename);
+                if (mzs > 0)
+                {
+                    string md5 = BitConverter.ToString(GetMD5(filename)).Replace("-","").ToLower();
+                    Console.WriteLine("{0}\n  MD5: {1} MZs: {2} ", filename, md5, mzs);
+                }
             }
+        }
+
+        byte[] GetMD5(string file)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(file))
+                {
+                    return md5.ComputeHash(stream);
+                }
+            }
+        }
+
+        int CountMZs(string file)
+        {
+            int count = 0;
+
+            const int chunkSize = 65536;
+            byte[] chunk = new byte[chunkSize];
+            int offset = 0;
+            using (var stream = File.OpenRead(file))
+            {
+                while (stream.Read(chunk, offset, chunkSize) > 0)
+                {
+                    string s = System.Text.Encoding.ASCII.GetString(chunk);
+                    if (s.Contains("MZ"))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
         }
     }
 }
